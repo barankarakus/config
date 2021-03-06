0f3ebd5b3f2eb97d6fd4c3e2e675508bddd0c6d0
baran Karakus <baran@hudson-trading.com>
2021-03-06 11:30:27 -0500

    .zsh: Documenting utils.sh

--- .zsh/utils.sh
+++ .zsh/utils.sh
@@ -9,6 +9,12 @@ U_add_to_PATH_if_exists () {
     [ -d $1 ] && export PATH="$1:$PATH"
 }
 
+# Using the built-in `command` command (instead of, say, `which`) for two
+# reasons:
+# 1. It's built-in so FAST.
+# 2. Its behaviour is defined by the POSIX standard so it's portable; see here
+#    for the specification:
+#    https://pubs.opengroup.org/onlinepubs/9699919799/utilities/command.html
 U_command_exists () {
     command -v $1 &> /dev/null
 }
