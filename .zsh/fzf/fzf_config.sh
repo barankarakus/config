7beebfef9f1be9f5894eb00f88566e4488fef975
baran Karakus <baran@hudson-trading.com>
2021-03-09 16:34:26 -0500

    Better fzf

--- .zsh/fzf/fzf_config.sh
+++ .zsh/fzf/fzf_config.sh
@@ -1,25 +1,13 @@
 export FZF_DEFAULT_OPTS="--height 70% --reverse --border --ansi"
 export FZF_DEFAULT_COMMAND="ag -g ''"
 
-if U_command_exists bat; then
-    Rg () {
-        rg $@ '' | fzf --prompt='Rg> ' --bind "change:reload:rg $@ {q} || true" --phony \
-                    --preview="$ZDOTDIR/fzf/preview.sh {}" \
-                    --preview-window='up:50%:hidden' --bind 'ctrl-/:toggle-preview'
-    }
-else
-    Rg () {
-        rg $@ '' | fzf --prompt='Rg> ' --bind "change:reload:rg $@ {q} || true" --phony
-    }
-fi
-
-if U_command_exists bat; then
-    FILES_PREVIEW_COMMAND="bat --style=numbers --color=always"
-else
-    FILES_PREVIEW_COMMAND="cat"
-fi
+Rg () {
+    rg $@ '' | fzf --prompt='Rg> ' --bind "change:reload:rg $@ {q} || true" --phony \
+                --preview="$ZDOTDIR/fzf/preview.sh {}" \
+                --preview-window='up:50%:hidden' --bind 'ctrl-/:toggle-preview'
+}
 
 Files () {
-    ag $@ -g '' | fzf --prompt=$PWD/ --preview=$FILES_PREVIEW_COMMAND' {}' --preview-window='up:50%:hidden' \
+    ag $@ -g '' | fzf --prompt=$PWD/ --preview="$ZDOTDIR/fzf/preview.sh {}" --preview-window='up:50%:hidden' \
                       --bind 'ctrl-/:toggle-preview'
 }
