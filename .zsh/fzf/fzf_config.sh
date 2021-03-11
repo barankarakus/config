a0d1ed3b4bcc2ad2499ba3389f1d8f9fac9c92f6
barankarakus <barankarakus@hotmail.co.uk>
2021-03-11 02:35:22 +0000

    Improved fzf set-up

--- .zsh/fzf/fzf_config.sh
+++ .zsh/fzf/fzf_config.sh
@@ -2,12 +2,24 @@ export FZF_DEFAULT_OPTS="--height 70% --reverse --border --ansi"
 export FZF_DEFAULT_COMMAND="ag -g ''"
 
 Rg () {
-    rg $@ --line-number '' | fzf --prompt='Rg> ' --bind "change:reload:rg $@ {q} || true" --phony \
-                --preview="$ZDOTDIR/fzf/preview.sh {}" \
-                --preview-window='up:50%:hidden' --bind 'ctrl-/:toggle-preview'
+    # Need this TEMP variable because of shell quoting bullshit
+    TEMP=$@
+    # Making sure to always pass in --line-number flag, irrespective of default
+    # arguments set up in .ripgreprc, because this is needed for the preview
+    # window
+    rg  $@  --line-number '' \
+    |&  fzf --prompt='Rg> ' \
+            --bind "change:reload:rg $TEMP --line-number {q} || true" \
+            --phony \
+            --preview="$ZDOTDIR/fzf/preview.sh {}" \
+            --preview-window='up:50%:hidden' --bind 'ctrl-/:toggle-preview'
+    unset TEMP
 }
 
 Files () {
-    ag $@ -g '' | fzf --prompt=$PWD/ --preview="$ZDOTDIR/fzf/preview.sh {}" --preview-window='up:50%:hidden' \
-                      --bind 'ctrl-/:toggle-preview'
+    ag  $@  -g '' \
+    |&  fzf --prompt=$PWD/ \
+            --preview="$ZDOTDIR/fzf/preview.sh {}" \
+            --preview-window='up:50%:hidden' \
+            --bind 'ctrl-/:toggle-preview'
 }
