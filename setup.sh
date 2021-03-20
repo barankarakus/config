44004c3b9ebf35fed6ae84cf6a72ffad0d2e90c3
baran Karakus <baran@hudson-trading.com>
2021-03-20 13:13:39 -0400

    Update

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 # Set CONFIGDIR to directory of this script
 
 export CONFIGDIR=$(realpath $(dirname $0))
-echo "CONFIGDIR=$CONFIGDIR"
+echo "Detected CONFIGDIR=$CONFIGDIR"
+echo
 
 # Create zshrc
 
@@ -16,6 +17,7 @@ echo "export CONFIGDIR=$CONFIGDIR" >> ~/.zshrc
 echo 'source $CONFIGDIR/zsh/zshrc' >> ~/.zshrc
 
 echo "Created ~/.zshenv"
+echo
 
 # Create vanilla vimrc
 
@@ -28,6 +30,7 @@ echo 'let &rtp = $CONFIGDIR . "/vim" . "," . &rtp . "," . $CONFIGDIR . "/vim/aft
 echo 'source $CONFIGDIR/vim/vimrc' >> ~/.vimrc
 
 echo "Created ~/.vimrc"
+echo
 
 # Create neovim vimrc
 
@@ -40,6 +43,7 @@ echo 'let &rtp = $CONFIGDIR . "/vim" . "," . &rtp . "," . $CONFIGDIR . "/vim/aft
 echo 'source $CONFIGDIR/vim/vimrc' >> ~/.config/nvim/init.vim
 
 echo "Created ~/.config/nvim/init.vim"
+echo
 
 # Symlink .tmux.conf
 
@@ -51,21 +55,26 @@ fi
 /usr/bin/env ln -s $CONFIGDIR/tmux/tmux.conf ~/.tmux.conf
 
 echo "Created symlink ~/.tmux.conf -> $CONFIGDIR/tmux/tmux.conf"
+echo
 
 # Download zplug if not already downloaded
 
 ZPLUG_HOME=$CONFIGDIR/zsh/zplug
-if [[ ! -d $ZPLUG_HOME ]]; then
+if [[ ! -f $ZPLUG_HOME/init.zsh ]]; then
     echo "Downloading zplug to $ZPLUG_HOME"
     git clone https://github.com/zplug/zplug $CONFIGDIR/zsh/zplug
     echo "Downloaded zplug"
+else
+    echo "Detected zplug installation at $ZPLUG_HOME"
 fi
+echo
 
 # Install zsh plugins
 
 echo "Installing zsh plugins"
 source $CONFIGDIR/zsh/zshplugins.sh
 zplug install
+echo
 
 # Download vim-plug if not already downloaded
 
@@ -75,12 +84,16 @@ if [[ ! -f $VIMPLUG_FILE ]]; then
     curl -fLo $VIMPLUG_FILE --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     echo "Downloaded vimplug"
+else
+    echo "Detected vimplug installation at $VIMPLUG_FILE"
 fi
+echo
 
 # Run :PlugInstall within vim
 
 echo "Installing vim plugins"
 /usr/bin/env vim +PlugInstall +qa!
+echo
 
 # Finally, load zshrc
 source ~/.zshrc
